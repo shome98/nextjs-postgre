a873b19544bc478f591015b4f4c25eec99da0e7e
Sowparna Shome <soupornoshome@gmail.com>
2024-11-08 00:31:38 +0530

    styles check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,6 @@ export default function Home() {
   const [isClient, setIsClient] = useState(false);
   useEffect(()=>setIsClient(true),[])
   return (
-    <><h1>{isClient ? 'This is never prerendered' : 'Prerendered'}</h1></>
+    <><h1 className="mt-2 ml-16 text-3xl px-9 py-9">{isClient ? 'This is never prerendered' : 'Prerendered'}</h1></>
   );
 }
